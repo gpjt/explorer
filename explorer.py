4ee79b36a484d5a038bd793a779dc1589605c3e7
Giles Thomas <giles@giles.net>
2009-09-30 18:46:39 +0100

    Reintroduced orbital speed for default spaceship position Broke dashboard out into a separate class.

--- explorer.py
+++ explorer.py
@@ -18,7 +18,7 @@ class SurroundingSky(object):
     
     def __init__(self):
         self.radius = 10000000
-        self.texture = LoadTexture("gigapixel-milky-way.jpg")    
+        self.texture = LoadTexture("gigapixel-milky-way.jpg")
 
 
     def draw(self):
@@ -213,7 +213,7 @@ class Universe(object):
 
         # Some interesting values:
         #  Distant orbit
-        self.userSpaceship = UserSpaceship(earth.offset(0, 0, 19999), (0, 0, 0)) #-4.4667
+        self.userSpaceship = UserSpaceship(earth.offset(0, 0, 19999), (-4.4667, 0, 0))
         #  Roughly as high as the ISS
         # self.userSpaceship = UserSpaceship(earth.offset(0, 0, earth.radius + 340), (-7.73, 0, 0))
         #  GEO
@@ -308,8 +308,82 @@ class Universe(object):
                 vY + aY * time,
                 vZ + aZ * time
             )
-                
-                
+
+
+class Dashboard(object):
+
+    def __init__(self, userSpaceship):
+        self.userSpaceship = userSpaceship
+        self.font = pygame.font.Font(None, 20)
+    
+    
+    def _renderText(self, text):
+        textSurface = self.font.render(text, True, (0, 255, 0, 255), (0, 0, 0, 0))
+        textData = pygame.image.tostring(textSurface, "RGBA", True)
+        return textSurface, textData        
+
+    def _normalise(self, number):            
+        if number < 0.1:
+            return locale.format("%.1f", number * 1000, True)
+        elif number < 1:
+            return locale.format("%.0f", number * 1000, True)
+        elif number < 10:
+            return "%sk" % locale.format("%.2f", number, True)
+        elif number < 100:
+            return "%sk" % locale.format("%.1f", number * 1000, True)
+        return "%sk" % locale.format("%.0f", number, True)
+
+
+    def draw(self, screenWidth, screenHeight):
+        topOffset = 10
+        rightOffset = 10
+        wGap = 10
+        leading = 3
+
+        data = [
+            ("Relative to:", self.relativeTo.name),
+            ("Distance:", "%sm" % self._normalise(self.userSpaceship.scalarDistanceRelativeTo(self.relativeTo))),
+            ("Velocity:", "%sm/s" % self._normalise(self.userSpaceship.scalarVelocityRelativeTo(self.relativeTo))),
+        ]
+
+        # Draw text, adapted from http://code.activestate.com/recipes/115418/
+        glPushMatrix()
+        
+        glViewport(0, 0, screenWidth, screenHeight)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0.0, screenWidth - 1.0, 0.0, screenHeight - 1.0, -1.0, 1.0)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        
+        renderedData = []
+        maxLeft = 0
+        maxRight = 0
+        maxHeight = 0
+        for leftLabel, rightLabel in data:
+            leftRender = self._renderText(leftLabel)
+            rightRender = self._renderText(rightLabel)
+            renderedData.append((leftRender, rightRender))
+            maxLeft = max(maxLeft, leftRender[0].get_width())
+            maxRight = max(maxRight, rightRender[0].get_width())
+            maxHeight = max(maxHeight, leftRender[0].get_height(), rightRender[0].get_height())
+
+        for rowIndex, ((leftSurface, leftData), (rightSurface, rightData)) in enumerate(renderedData):
+            glRasterPos2i(
+                screenWidth - rightOffset - min(maxLeft, leftSurface.get_width()) - wGap - maxRight,
+                screenHeight - topOffset - (rowIndex + 1) * (maxHeight + leading)
+            )
+            glDrawPixels(leftSurface.get_width(), leftSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, leftData)
+
+            glRasterPos2i(
+                screenWidth - rightOffset - maxRight,
+                screenHeight - topOffset - (rowIndex + 1) * (maxHeight + leading)
+            )
+            glDrawPixels(rightSurface.get_width(), rightSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, rightData)
+
+        glPopMatrix()
+        
+
 
 class UI(object):
 
@@ -426,77 +500,6 @@ class UI(object):
             self.done = True
 
 
-    def _renderText(self, font, text):
-        textSurface = font.render(text, True, (0, 255, 0, 255), (0, 0, 0, 0))
-        textData = pygame.image.tostring(textSurface, "RGBA", True)
-        return textSurface, textData        
-
-
-    def drawDashboard(self):
-        topOffset = 10
-        rightOffset = 10
-        wGap = 10
-        leading = 3
-
-        def Normalise(number):            
-            if number < 0.1:
-                return locale.format("%.1f", number * 1000, True)
-            elif number < 1:
-                return locale.format("%.0f", number * 1000, True)
-            elif number < 10:
-                return "%sk" % locale.format("%.2f", number, True)
-            elif number < 100:
-                return "%sk" % locale.format("%.1f", number * 1000, True)
-            return "%sk" % locale.format("%.0f", number, True)
-
-        data = [
-            ("Relative to:", self.dashboardRelativeTo.name),
-            ("Distance:", "%sm" % Normalise(self.universe.userSpaceship.scalarDistanceRelativeTo(self.dashboardRelativeTo))),
-            ("Velocity:", "%sm/s" % Normalise(self.universe.userSpaceship.scalarVelocityRelativeTo(self.dashboardRelativeTo))),
-        ]
-
-        # Draw text, adapted from http://code.activestate.com/recipes/115418/
-        glPushMatrix()
-        
-        screenWidth, screenHeight = self.resolution
-        glViewport(0, 0, screenWidth, screenHeight)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(0.0, screenWidth - 1.0, 0.0, screenHeight - 1.0, -1.0, 1.0)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-
-        font = pygame.font.Font(None, 20)
-        renderedData = []
-        maxLeft = 0
-        maxRight = 0
-        maxHeight = 0
-        for leftLabel, rightLabel in data:
-            leftRender = self._renderText(font, leftLabel)
-            rightRender = self._renderText(font, rightLabel)
-            renderedData.append((leftRender, rightRender))
-            maxLeft = max(maxLeft, leftRender[0].get_width())
-            maxRight = max(maxRight, rightRender[0].get_width())
-            maxHeight = max(maxHeight, leftRender[0].get_height(), rightRender[0].get_height())
-
-        for rowIndex, ((leftSurface, leftData), (rightSurface, rightData)) in enumerate(renderedData):
-            glRasterPos2i(
-                screenWidth - rightOffset - min(maxLeft, leftSurface.get_width()) - wGap - maxRight,
-                screenHeight - topOffset - (rowIndex + 1) * (maxHeight + leading)
-            )
-            glDrawPixels(leftSurface.get_width(), leftSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, leftData)
-
-            glRasterPos2i(
-                screenWidth - rightOffset - maxRight,
-                screenHeight - topOffset - (rowIndex + 1) * (maxHeight + leading)
-            )
-            glDrawPixels(rightSurface.get_width(), rightSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, rightData)
-
-        glPopMatrix()
-        
-
-
-
     def draw(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
@@ -507,7 +510,7 @@ class UI(object):
         glRotatef(math.degrees(-self.cameraPitch), 1, 0, 0)
         
         self.universe.draw()
-        self.drawDashboard()
+        self.dashboard.draw(*self.resolution)
 
 
     def main(self):
@@ -529,7 +532,8 @@ class UI(object):
             self.resize(*self.resolution)
             self.initGL()
             self.universe = Universe()
-            self.dashboardRelativeTo = self.universe.initialDashboardRelativeTo
+            self.dashboard = Dashboard(self.universe.userSpaceship)
+            self.dashboard.relativeTo = self.universe.initialDashboardRelativeTo
 
             frames = 0
             ticks = pygame.time.get_ticks()
